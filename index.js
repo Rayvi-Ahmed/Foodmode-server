9e3fa6c56b5286b1cb24053e3e0d2cff61fe83ec
Rayvi-Ahmed <rayvi73@gmail.com>
2023-05-04 23:14:50 +0600

    add recipe json

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port= process.env.PORT|| 5000;
 app.use(cors())
 
 const Chefs= require('./data/chefs.json')
+const recipe=require('./data/recipe.json')
 
 
 app.get('/',(req,res)=>{
@@ -18,13 +19,17 @@ app.get('/chefs',(req,res)=>{
 res.send(Chefs)
 })
 
+app.get('/recipe',(req,res)=>{
+res.send(recipe)
+})
+
 app.get('/chefs/:id',(req,res)=>{
   const id= req.params.id;
   const item = Chefs?.find(ch=>ch.id==id)
   console.log(item)
   res.send(item)
 })
-
+ 
 
 app.listen(port,()=>{
     console.log(`foodie recipes is continueing ${port}`)
